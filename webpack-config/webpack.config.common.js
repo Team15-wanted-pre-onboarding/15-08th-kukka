1435dea1847d3f6214c060ca6bbc3b09351d18ac
pomeranian91 <forking91@gmail.com>
2022-02-22 05:20:15 +0900

    Add:팝업창이벤트,풋터반응형디자인

--- webpack-config/webpack.config.common.js
+++ webpack-config/webpack.config.common.js
@@ -1,77 +1,95 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === 'PRODUCTION';
+const isProduction = process.env.NODE_ENV === "PRODUCTION";
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                  "style-loader", 
-                  "css-loader", 
-                  "sass-loader" 
-                ]  
-            }, {
-                test: /\.hbs$/,
-                use: ['handlebars-loader']
-            }, {
-                test: /\.(png|jpg?g|gif)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name() {
-                            if(!isProduction) {
-                                return '[path][name].[ext]';
-                            } 
-                            return '[contenthash].[ext]';
-                        },
-                        publicPath: 'img/',
-                        outpath: 'img/'
-                    }
-                }]
-            }, {
-                test: /\.svg/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }]
-            }, {
-                test: / .js/,
-                exclude: /node-modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'kukka-project',
-            template: './template.hbs',
-            meta: {
-                author: "CHOI",
-                description: "This site is where people see a various of flower's info",
-                viewport: 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1'
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+      {
+        test: /\.(png|jpg?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name() {
+                if (!isProduction) {
+                  return "[path][name].[ext]";
+                }
+                return "[contenthash].[ext]";
+              },
+              publicPath: "img/",
+              outpath: "img/",
             },
-            minify: isProduction ? {
-                collapseWhitespace: true,
-                useShortDoctype: true,
-                removeScriptTypeAttributes: true
-            } : false
-        }),
-        new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-            IS_PRODUCTION: isProduction
-        }),
-        new MiniCssExtractPlugin()
-    ]
-}
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: / .js/,
+        exclude: /node-modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[hash].[ext]",
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "kukka-project",
+      template: "./template.hbs",
+      meta: {
+        author: "CHOI",
+        description: "This site is where people see a various of flower's info",
+        viewport: "width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1",
+      },
+      minify: isProduction
+        ? {
+            collapseWhitespace: true,
+            useShortDoctype: true,
+            removeScriptTypeAttributes: true,
+          }
+        : false,
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      IS_PRODUCTION: isProduction,
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
