02b6e164664d837c25d73cc357b49900f30bc00e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-22 03:57:34 +0900

    Add: nav scroll event

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,21 @@
 import "./Nav.scss";
 
+const headerNav = document.querySelector(".nav-view");
+
 const Nav = () => {
-  console.log("this is Nav");
+  const scrollClassAdd = (e) => {
+    //console.log(window.scrollY);
+    console.dir(e.path[1].scrollY);
+    if (window.scrollY === 0) {
+      headerNav.classList.remove("has_shadow");
+      console.log("맨위에");
+    } else {
+      headerNav.classList.add("has_shadow");
+      console.log("스크롤중");
+    }
+    headerNav.classList.add("has_shadow");
+  };
+  window.addEventListener("scroll", scrollClassAdd);
 };
 
 export default Nav;
