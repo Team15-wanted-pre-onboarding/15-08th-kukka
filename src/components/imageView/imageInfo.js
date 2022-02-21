6e3570fabff40cb31b76cb929d950b92ceffd6e0
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-22 04:13:39 +0900

    Refactor: image 코드 정리

---
+++ src/components/imageView/imageInfo.js
@@ -0,0 +1,26 @@
+export const imageInfo = [
+  {
+    id: 1,
+    image:
+      "https://kukka-2-media-123.s3.amazonaws.com/media/class-name/ContentsTopBanner/3/class_2022_home_web_R0tReO1.jpeg",
+    title: "취미 클래스(총 4회)",
+    day: "2/21(월)~2/27(일)",
+    money: 299000,
+  },
+  {
+    id: 2,
+    image:
+      "https://kukka-2-media-123.s3.amazonaws.com/media/class-name/ClassProductGroup/106/img_%EC%98%A4%EC%95%84%EC%8B%9C%EC%8A%A4-%EB%A6%AC%EC%8A%A4.jpg",
+    title: "오아시스 리스",
+    day: "2/21(월)~2/27(일)",
+    money: 79000,
+  },
+  {
+    id: 3,
+    image:
+      "https://kukka-2-media-123.s3.amazonaws.com/media/class-name/ClassProductGroup/107/img_%ED%81%B4%EB%9E%98%EC%8B%9D-%ED%95%B8%EB%93%9C%ED%83%80%EC%9D%B4%EB%93%9C.jpg",
+    title: "클래식 핸드타이드",
+    day: "2/28(월)~3/6(일)",
+    money: 79000,
+  },
+];
