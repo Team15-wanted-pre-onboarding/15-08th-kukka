78e1123fa1346232244367347009dcc091f16021
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-22 20:20:25 +0900

    Add: Nav바 기능 디자인 ing

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,17 +3,12 @@ import "./Nav.scss";
 const headerNav = document.querySelector(".nav-view");
 
 const Nav = () => {
-  const scrollClassAdd = (e) => {
-    //console.log(window.scrollY);
-    console.dir(e.path[1].scrollY);
+  const scrollClassAdd = () => {
     if (window.scrollY === 0) {
       headerNav.classList.remove("has_shadow");
-      console.log("맨위에");
     } else {
       headerNav.classList.add("has_shadow");
-      console.log("스크롤중");
     }
-    headerNav.classList.add("has_shadow");
   };
   window.addEventListener("scroll", scrollClassAdd);
 };
